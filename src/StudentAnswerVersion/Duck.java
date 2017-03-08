756ad16e7bb39fa39d39b4a86de7175e6b5ef421
knyghtspup <knyghtspup@10.30.16.77>
2017-03-08 09:12:56 -0600

    Created methods in 'Duck' class, included setters and getters, and made setters final 9:12

--- src/StudentAnswerVersion/Duck.java
+++ src/StudentAnswerVersion/Duck.java
@@ -11,4 +11,42 @@ package StudentAnswerVersion;
  */
 public abstract class Duck {
     
+    private Flyable flyable;
+    private Quackable quackable;
+    private Display display;
+    
+    public Duck () {}
+
+    public Flyable getFlyable() {
+        return flyable;
+    }
+
+    public final void setFlyable(Flyable flyable) {
+        this.flyable = flyable;
+    }
+
+    public Quackable getQuackable() {
+        return quackable;
+    }
+
+    public final void setQuackable(Quackable quackable) {
+        this.quackable = quackable;
+    }
+
+    public Display getDisplay() {
+        return display;
+    }
+
+    public final void setDisplay(Display display) {
+        this.display = display;
+    }
+    
+    public final void performFly() {
+        flyable.fly();
+    }
+    
+    public final void performQuack() {
+        quackable.quack();
+    }
+    
 }
